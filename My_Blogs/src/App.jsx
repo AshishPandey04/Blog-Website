5e8633670d19a3fe441cfb8df05e41bfdf5e3fd6
AshishPandey04 <0203ashish2004@gmail.com>
2024-12-02 19:29:41 +0530

    components

--- My_Blogs/src/App.jsx
+++ My_Blogs/src/App.jsx
@@ -1,16 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
+
+import authService from "./appwrite/auth"
+import {login,logout} from './store/authSlice'
+import {Header,Footer} from "./components"
+
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading,setLoading]=useState(true)
+  const dispatch=useDispatch()
 
-  return (
-    <>
-      <h1>hiii i am pagal</h1>
-    </>
-  )
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+
+    })
+    .finally(()=> setLoading(false))
+  },[])
+  
+return !loading?(
+  <div className='min-h-screen flex flex-wrap content-between 
+  bg-orange-100'>
+    <div className='w-full block'>
+      <Header/>
+      <main>
+        {/* <outlet/> */}
+      </main>
+      <Footer/>
+    </div>
+  </div>
+):null
+// loading ka mssg bhi dikha sakte hain
 }
 
 export default App
