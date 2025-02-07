381122dabb3d021cf83436a5fa52d63c813da9cf
AshishPandey04 <0203ashish2004@gmail.com>
2025-02-07 12:03:41 +0530

    project

---
+++ src/components/index.js
@@ -0,0 +1,35 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Logo from "./Logo";
+import Container from './container/Container'
+import LogoutBtn from '../components/Header/LogoutBtn'
+import RTE from "./RTE";
+import Signup from './Signup'
+import Login from './Login'
+import PostForm from './PostForm'
+import PostCard from "./PostCard"
+import Protected from "./AuthLayout"
+import Button from "./Button"
+import Input from "./Input"
+import Select from "./Select"
+
+export {
+    Header,
+    Footer,
+    Logo,
+    Container,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    Protected,
+    Button,
+    Input,
+    Select,
+
+
+
+
+}
