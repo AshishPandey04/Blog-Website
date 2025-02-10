34fcec7d901a213301e3d05f61a5f5e1457869a8
AshishPandey04 <0203ashish2004@gmail.com>
2025-02-11 00:22:13 +0530

    update

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -4,8 +4,8 @@ const conf = {
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-    apiKey: String(import.meta.env.REACT_APP_API_KEY)
-    
+    // apiKey: String(import.meta.env.REACT_APP_API_KEY)
+    apiKey:"gpmeyicxqdjit37to225bplxtmw110kzes8rhdjngdwjfphg",
 }
 
 
